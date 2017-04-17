07b032ccac91c1776f83b2453935cb59a4e735f0
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-04-17 21:01:29 +0800

    add cpp project

---
+++ CCode/cmake-build-debug/CMakeFiles/CCode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CCode.dir/main.cpp.obj"
+  "CCode.pdb"
+  "CCode.exe"
+  "CCode.exe.manifest"
+  "libCCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
