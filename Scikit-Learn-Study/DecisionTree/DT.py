7b086643ef4a0e3bc3dcdd501ebf23c898973240
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-04-18 16:55:14 +0800

    ha

--- Scikit-Learn-Study/DecisionTree/DT.py
+++ Scikit-Learn-Study/DecisionTree/DT.py
@@ -7,3 +7,32 @@ reader = csv.reader(allElectronicData)
 headers = reader.next()
 
 print(headers)
+
+featureList = []
+labelList = []
+
+for row in reader:
+    labelList.append(row[len(row)-1])
+    rowDict = {}
+    for i in range(1, len(row) -1):
+        rowDict[headers[i]] = row[i]
+
+    featureList.append(rowDict)
+
+print(featureList)
+
+#vectorize feature
+vec = DictVectorizer()
+dummyX = vec.fit_transform(featureList).toarray()
+print("dummyX: " + str(dummyX))
+print(vec.get_feature_names())
+
+print("labelList: " + str(labelList))
+
+lb = preprocessing.LabelBinarizer()
+dummyY = lb.fit_transform(labelList)
+print ("dummyY: " + str(dummyY))
+
+clf = tree.DecisionTreeClassifier(criterion="entropy")
+clf = clf.fit(dummyY,dummyY)
+print ("clf: " + str(clf))
