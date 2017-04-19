8fe9de98cd5eeb1dffcb5b14dac593d117af9cce
supersonictan <376079374@qq.com>
2017-04-19 11:04:32 +0800

    Day1 ok

--- PyCode/python_study/Day1.py
+++ PyCode/python_study/Day1.py
@@ -10,9 +10,23 @@ import sys
     Part_3: 缩进在linux下tab是8个空格，win下是4个。
     Part_4: 查看变量类型：print type(var)
             强制类型转换: int("123")
+    Part_5: 
+	    python的true:True
+	    if age>50:
+            elif age=0:
+            else:
+    Part_6: 循环
+	     for i in range(1,100),会打印[1,99], 
+	     range也可以range(100), 
+	     range(1,100,2) 
+	     
 """
 
 
 #Part_2:
 name = raw_input("Input something")
 print "Hello " + name
+
+#Part_6:
+for i in range(0,10,2):
+    print i
