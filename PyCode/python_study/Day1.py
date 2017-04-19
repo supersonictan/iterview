f09064b8385363c136229674af4d72e9bcde3257
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-04-19 10:16:16 +0800

    update

---
+++ PyCode/python_study/Day1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+"""
+内容：
+    Part_1: 导入模块 improt是导入整个模块
+            from os improt *从某个模块导入特定方法。
+            别名: import moduleName as newName
+    Part_2: row_input("Please input ....")
+    Part_3: 缩进在linux下tab是8个空格，win下是4个。
+    Part_4: 查看变量类型：print type(var)
+            强制类型转换: int("123")
+"""
+
+
+#Part_2:
+name = raw_input("Input something")
+print "Hello " + name
