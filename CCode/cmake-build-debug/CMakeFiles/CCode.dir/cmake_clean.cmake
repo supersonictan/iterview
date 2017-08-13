6389cf82dd1b00f50f610d8ccbb3b54a763559f3
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-08-13 21:17:34 +0800

    adf

---
+++ CCode/cmake-build-debug/CMakeFiles/CCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CCode.dir/main.cpp.o"
+  "CCode.pdb"
+  "CCode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- CCode/main.cpp
+++ CCode/main.cpp
@@ -1,84 +1,90 @@
+#include <assert.h>
+#include <cstring>
+#include <string>
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
+/*
+ * 1. 实现strcpy函数
+ * 2. 实现strlen
+ * 3. 写String的成员函数
+ * */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+/*1 实现strcpy函数*/
+char* strcpy(char*strDest, const char*strSrc)
+{
+    assert((strDest!=NULL) && (strSrc !=NULL));
+    char* address = strDest;
+    while( (*strDest++ = *strSrc++) !='\0' )
+        NULL;//while体里面不做任何操作，就用了NULL;
 
+    return address ;
+}
 
-    vector<vector<uint32_t>> aliasTermsVec_seg1;
-    vector<vector<uint32_t>> aliasWeightsVec_seg1;
+/*2 实现strlen*/
+int my_strlen(const char* strSrc) {
+    if (strSrc == NULL) return 0;
 
-    vector<uint32_t> aliasTermsTotal;
-    aliasTermsTotal.push_back(2);
-    aliasTermsTotal.push_back(194180932);
-    aliasTermsTotal.push_back(134567);
-    aliasTermsTotal.push_back(3302234209);
-    aliasTermsTotal.push_back(47620);
-    aliasTermsTotal.push_back(3);
-    aliasTermsTotal.push_back(194180932);
-    aliasTermsTotal.push_back(134567);
-    aliasTermsTotal.push_back(3585031579);
-    aliasTermsTotal.push_back(116584);
-    aliasTermsTotal.push_back(1929370669);
-    aliasTermsTotal.push_back(27868);
-
-
-    vector<uint32_t>* tmpTermVec; //当前show的term vector
-    vector<uint32_t>* tmpWeightVec; //当前show的weight vector
-    uint32_t remainTerms = 0;
-    //2 194180932 134567 3302234209 47620 3 194180932 134567 3585031579 116584 1929370669 27868 2 2044737783 30354 194180932 134567 5 2068004672 53837 3370521906 36000 818911323 56218 3370521906 36000 2515168961 43056
-    for(size_t i=0; i<aliasTermsTotal.size(); ++i) {
-        if (remainTerms == 0) {
-            if (i != 0) {
-                aliasTermsVec_seg1.push_back(*tmpTermVec);
-                aliasWeightsVec_seg1.push_back(*tmpWeightVec);
-            }
-            remainTerms = aliasTermsTotal[i] * 2; //terms和权重
-            vector<uint32_t> tmp;
-            vector<uint32_t> tmp_w;
-            tmpTermVec = &tmp;
-            tmpWeightVec = &tmp_w;
-
-
-            continue;
-        }
-        if (remainTerms%2 == 0) { //term值
-            uint32_t termId = aliasTermsTotal[i];
-            (*tmpTermVec).push_back(termId);
-        } else { //weight value
-            uint32_t weight = aliasTermsTotal[i];
-            (*tmpWeightVec).push_back(weight);
-        }
-        remainTerms -= 1;
-        if (i == (aliasTermsTotal.size()-1) ){
-            aliasTermsVec_seg1.push_back(*tmpTermVec);
-            aliasWeightsVec_seg1.push_back(*tmpWeightVec);
-        }
+    int resLen = 0;
+    while ((*strSrc++) != '\0') {
+        resLen ++;
     }
-    cout<<aliasTermsVec_seg1[0][0]<<endl;
-    cout<<aliasTermsVec_seg1[1][1]<<endl;
+    return resLen;
+}
 
+/*3 String的成员函数*/
+class String
+{
+public:
+        String(const char *str = NULL); // 通用构造函数
+        String(const String &another); // 拷贝构造函数
+        ~String(); // 析构函数
+        //& operater = (const String &rhs); // 赋值函数
+private:
+        char* m_data; // 用于保存字符串
 
-
-
-    /*std::vector<int32_t >* v;
-    std::vector<vector<int32_t>> vec;
-
-    for (int i = 0; i < 5; ++i) {
-        std::vector<int32_t> tmpVec;
-        //tmpVec.push_back(i);
-        v = &tmpVec;
-        (*v).push_back(i);
-        vec.push_back(*v);
+};
+//通用构造函数
+String::String(const char*str)
+{
+    char* m_data;
+    if ( str == NULL ) // strlen在参数为NULL时会抛异常才会有这步判断
+    {
+        m_data = new char[1] ;
+        m_data[0] ='\0' ;
     }
-    cout<< vec[0][0]<<endl;
-    cout<< vec[1][0]<<endl;
-    cout<< vec[2][0]<<endl;
-    cout<< vec[3][0]<<endl;
-    cout<< vec[4][0]<<endl;
-    cout << vec.size()<<endl;*/
+    else
+    {
+        m_data = new char[strlen(str) +1];
+        strcpy(m_data, str);
+    }
+}
+//拷贝构造函数
+String::String(const String &another)
+{
+    char* m_data;
+    m_data = new char[strlen(another.m_data) +1];
+    strcpy(m_data, another.m_data);
+}
 
-    return 0;
+
+
+int main()
+{
+    vector<int> v ;
+    list<int> l;
+
+    v.reserve(2);
+    v.push_back(123);
+    v.push_back(345);
+    for (int i = 0; i < 100; ++i) {
+        v.push_back(i);
+        l.push_back(i);
+    }
+
+    for (vector<int>::iterator vi = v.begin(); vi != v.end()  ; vi++) {
+        cout << *vi;
+    }
 }
