238beb2ca375852a8e5d489d5fef2267d4c6ebfc
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-09-30 17:23:32 +0800

    asdf

---
+++ PriCode/src/main/java/interview/ali_2017/bit.java
@@ -0,0 +1,17 @@
+package interview.ali_2017;
+
+/**
+ * Created by ZeYu
+ * Date: 2017/9/30.
+ * Time: 17:20.
+ * DESC: say something
+ */
+public class bit {
+
+
+
+    /*交换两个数,不用第三个变量*/
+    public static void swap(int a, int b) {
+        //a ^ b ^ a = b
+    }
+}
