e9c6d603a0ad03c6b2861c4c0a03da94d8b93379
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-12-28 10:46:32 +0800

    udpate

---
+++ PyCode/youku_work/kubox_dii/long_query.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding=utf-8-*-
+import json,re,datetime
+import types
+import time
+
+
+
---
+++ PyCode/youku_work/tmp/gen_log.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#encoding=utf-8
+# -*- coding=utf-8-*-
+
+
+
+def read_query_file():
+    with open("person_name", 'r') as p, open("rel", 'rb') as r, open("result", 'w') as r:
+        for person in p:
+            for relation in r:
+                newLine = person.strip() + relation.strip()
+                r.write(newLine)
+                r.write('\n')
+
+
+
+if __name__ == '__main__':
+    read_query_file()
