26f92376ab93774001e78575371e3b008ded41fd
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2019-01-03 23:54:05 +0800

    update

--- PriCode/src/main/java/interview/ali_2019/TreeProblem.java
+++ PriCode/src/main/java/interview/ali_2019/TreeProblem.java
@@ -20,6 +20,9 @@ import java.util.*;
  * 107. 二叉树的层次遍历 II:自底向上 List<List<Integer>> levelOrderBottom(TreeNode root)
  * 104. 二叉树的最大深度: int maxDepth(TreeNode root)
  * 111. 二叉树的最小深度: int minDepth(TreeNode root)
+ * 236. 二叉树的最近公共祖先
+ * 958. 二叉树的完全性检验
+ * 222. 完全二叉树的节点个数
  *
  */
 
@@ -275,10 +278,97 @@ public class TreeProblem {
         return res;
     }
 
+    // 236. 二叉树的最近公共祖先
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        if (root == null) return null;
+        if (root == A || root == B) return root;
 
+        TreeNode left = lowestCommonAncestor(root.left, A, B);
+        TreeNode right = lowestCommonAncestor(root.right, A, B);
 
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        return right;
+    }
 
+    // 958. 二叉树的完全性检验
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) return false;
 
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        boolean mustNoChild = false;
+        boolean res;
+
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+
+            if (mustNoChild) {
+                if (cur.left != null || cur.right != null) {
+                    return false;
+                }
+            } else {
+                if (cur.left != null && cur.right != null) {
+                    q.offer(cur.left);
+                    q.offer(cur.right);
+                } else if (cur.left != null && cur.right == null) {
+                    q.offer(cur.left);
+                    mustNoChild = true;
+                } else if (cur.left == null && cur.right == null) {
+                    mustNoChild = true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // 获取左子树的高度(其实是最左侧分支）
+    public int getLeftHeight(TreeNode root) {
+        int count = 0;
+        while (root != null) {
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+
+    // 222. 完全二叉树的节点个数
+    /**
+     *二叉树的第n层最多为2n个节点,二叉树最多有2n+1−1个节点
+     * 基于这两个性质，我们可以这样思考。
+     * 如果右子树的高度等于整棵二叉树的高度-1的话，
+     * 那么左子树一定是一棵满二叉树，这个时候我们就很容易的计算出总结点数nodes=2**(h)-1 + 1 +右子树节点数（这里的+1表示root节点）。
+     * 如果右子树的高度不等于整棵二叉树的高度-1的话，那么左子树不一定是一棵满二叉树，但是有一点可以确定，右子树一定是一棵满二叉树，
+     * 这个时候我们就很容易的计算出总结点数nodes=2**(h-1)-1 + 1 +左子树节点数（这里的+1表示root节点）。
+     * 根据这个思路我们只要不断循环下去直到root==None结束
+     */
+    public int getRightHeight(TreeNode root) {
+        int count = 0;
+        while (root != null) {
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if (leftHeight == rightHeight) {
+            // 表示是满二叉树，二叉树的节点数直接由公式2^n-1得到
+            // leftHeight即为层数， 1 << leftHeight使用位运算计算2^leftHeight，效率更高
+            // 注意(1 << leftHeight) - 1 的括号必须有！！
+            return (1 << leftHeight) - 1;
+        } else {
+            // 若该二叉树不是满二叉树，递归的调用该方法，计算左子树和右子树的节点数
+            return countNodes(root.left) + countNodes(root.right) + 1;
+        }
+    }
 
 
 
