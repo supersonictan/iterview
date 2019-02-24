84352244b409779d56b0e04b32be3f3e40ebf11a
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2019-02-24 16:42:51 +0800

    update

--- PriCode/src/main/java/interview/ali_2019/DP2019.java
+++ PriCode/src/main/java/interview/ali_2019/DP2019.java
@@ -294,7 +294,9 @@ public class DP2019 {
 
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
             }
             resVal = Math.max(resVal, dp[i]);
         }
